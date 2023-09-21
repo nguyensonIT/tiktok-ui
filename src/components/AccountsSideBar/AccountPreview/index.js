643b3beba89d6bab43a7b7ea2556cbdc85eacf62
Ulman-huy <buihuy.lh@gmail.com>
2023-09-21 14:17:30 +0700

    Config service

--- src/components/AccountsSideBar/AccountPreview/index.js
+++ src/components/AccountsSideBar/AccountPreview/index.js
@@ -4,54 +4,49 @@ import Button from "../../Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import Image from "../../Image";
+import { fetchFollowUser } from "../../../services/followingAccountsService";
 
 const cx = classNames.bind(styles);
 function AccountPreview({ data = [], isPreviewInVideo = false }) {
-    return (
-        <div className={cx("wrapper")}>
-            <header className={cx("header")}>
-                <Image
-                    className={cx("avatar")}
-                    src={data.avatar}
-                    alt={data.nickname}
-                />
-                <div>
-                    <Button primary className={cx("btn-follow")}>
-                        Follow
-                    </Button>
-                </div>
-            </header>
-            <div className={cx("info-preview")}>
-                <strong className={cx("nickname")}>{data.nickname}</strong>
-                <span>
-                    {data.tick && (
-                        <FontAwesomeIcon
-                            icon={faCheckCircle}
-                            className={cx("check-icon")}
-                        />
-                    )}
-                </span>
-                <h4
-                    className={cx("name")}
-                >{`${data.first_name} ${data.last_name}`}</h4>
-                <p className={cx("footer")}>
-                    <strong className={cx("number")}>
-                        {data.followers_count}{" "}
-                    </strong>
-                    <span className={cx("text-footer")}>Follower</span>
-                    <strong className={cx("number")}>
-                        {data.likes_count}{" "}
-                    </strong>
-                    <span className={cx("text-footer")}>Likes</span>
-                </p>
-            </div>
-            {isPreviewInVideo && (
-                <div className={cx("content-bio")}>
-                    <p className={cx("content-bio-text")}>{data.bio}</p>
-                </div>
-            )}
+  const handleFollow = async () => {
+    const resp = await fetchFollowUser(data?.id);
+    console.log({ resp });
+  };
+  return (
+    <div className={cx("wrapper")}>
+      <header className={cx("header")}>
+        <Image className={cx("avatar")} src={data.avatar} alt={data.nickname} />
+        <div>
+          <Button onClick={handleFollow} primary className={cx("btn-follow")}>
+            Follow
+          </Button>
         </div>
-    );
+      </header>
+      <div className={cx("info-preview")}>
+        <strong className={cx("nickname")}>{data.nickname}</strong>
+        <span>
+          {data.tick && (
+            <FontAwesomeIcon
+              icon={faCheckCircle}
+              className={cx("check-icon")}
+            />
+          )}
+        </span>
+        <h4 className={cx("name")}>{`${data.first_name} ${data.last_name}`}</h4>
+        <p className={cx("footer")}>
+          <strong className={cx("number")}>{data.followers_count} </strong>
+          <span className={cx("text-footer")}>Follower</span>
+          <strong className={cx("number")}>{data.likes_count} </strong>
+          <span className={cx("text-footer")}>Likes</span>
+        </p>
+      </div>
+      {isPreviewInVideo && (
+        <div className={cx("content-bio")}>
+          <p className={cx("content-bio-text")}>{data.bio}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default AccountPreview;
--- src/services/followingAccountsService.js
+++ src/services/followingAccountsService.js
@@ -1,28 +1,39 @@
 import * as request from "../utils/request";
+const token = localStorage.getItem("token");
+
 export const following = async (token, page) => {
-    try {
-        const response = await request.get("me/followings", {
-            params: {
-                page,
-            },
-            headers: {
-                Authorization: "Bearer " + token,
-            },
-        });
-        return response;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const response = await request.get("me/followings", {
+      params: {
+        page,
+      },
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 };
 export const followUser = async (token, id) => {
-    try {
-        const response = await request.post(`users/${id}/follow`, {
-            headers: {
-                Authorization: "Bearer " + token,
-            },
-        });
-        return response;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const response = await request.post(`users/${id}/follow`, {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchFollowUser = async (id) => {
+  const obj = {
+    path: `users/${id}/follow`,
+    token: token,
+  };
+  const resp = await request.POST(obj);
+  return resp;
 };
